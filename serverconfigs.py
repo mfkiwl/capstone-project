749ddce289e79632c72429ebea7f1d854be1eff1
Shiva Nathan <16snathan@gmail.com>
2020-02-13 21:04:51 -0500

    Added Shiva Nathan's Macbook Pro IP Addresses based on current wifi network

--- serverconfigs.py
+++ serverconfigs.py
@@ -1,4 +1,7 @@
+import subprocess
 HEADER_LENGTH = 10
-SERVER_IP = "127.0.0.1"
+# on CMU-SECURE, SERVER_IP = "127.0.0.1" 
+# on Nirvana, SERVER_IP = "10.0.0.55" 
+SERVER_IP = "10.0.0.55"
 SERVER_PORT = 1234
 BYTE_FORMAT = 'utf-8'
