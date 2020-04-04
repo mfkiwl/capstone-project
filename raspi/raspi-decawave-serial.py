c9c283c2cdd011aace2beb5ade79b1eaa64bd3ac
Shiva Nathan <16snathan@gmail.com>
2020-04-04 02:16:54 -0400

    Fixed bug where readline wasn't being called correctly

--- raspi/raspi-decawave-serial.py
+++ raspi/raspi-decawave-serial.py
@@ -4,7 +4,7 @@ try:
     with serial.Serial('/dev/ttyACM0', 115200, timeout=0) as ser:
         sio = io.TextIOWrapper(io.BufferedRWPair(ser, ser))
         while True:
-            print(sio.readline)
+            print(sio.readline())
 except KeyboardInterrupt:
     pass
 
