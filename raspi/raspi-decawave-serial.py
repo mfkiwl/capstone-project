4a1ba747a6fd1da1df4c18eb229d357c7c5a67d5
Shiva Nathan <16snathan@gmail.com>
2020-04-04 01:20:51 -0400

    Adding software for Raspi-Decawave communications

---
+++ raspi/raspi-decawave-serial.py
@@ -0,0 +1,11 @@
+import serial
+import io
+try:
+    with serial.Serial('/dev/ttyACM0', 115200, timeout=0) as ser:
+        sio = io.TextIOWrapper(io.BufferedRWPair(ser, ser))
+        while True:
+            print(sio.readline)
+except KeyboardInterrupt:
+    pass
+
+
