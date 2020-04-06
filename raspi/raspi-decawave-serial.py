7b1946d2d62724e3baea63d6d3728041ddc2f17b
Shiva Nathan <svnathan@andrew.cmu.edu>
2020-04-06 07:55:36 +0100

    Revert "Replaced BufferedRWPair with BufferedRandom since the former does not try to synchronize access to its stream"
    This reverts commit 1a443782d2eb1bf7565da55df7a2b4bea5c5e40e.

--- raspi/raspi-decawave-serial.py
+++ raspi/raspi-decawave-serial.py
@@ -8,7 +8,7 @@ from timeit import timeit
 def forwardLine(fh,sio, pointMarker): 
     line = sio.readline()
     fh.write(line)
-    if line.startswith(pointMarker):
+    if line.count(pointMarker) > 0:
         fh.write("RPI_rx_ts_nanosec:" + str(time.clock_gettime_ns(time.CLOCK_MONOTONIC_RAW)) + "\r\n")
 
 def main(filename,pointMarker="Reception"):
@@ -16,7 +16,7 @@ def main(filename,pointMarker="Reception"):
     print("filename opened!")
     try:
         with serial.Serial('/dev/ttyACM0', 115200, timeout=0) as ser:
-            sio = io.TextIOWrapper(io.BufferedRandom(ser, ser))
+            sio = io.TextIOWrapper(io.BufferedRWPair(ser, ser))
             while True:
                 forwardLine(fh,sio,pointMarker)
     except KeyboardInterrupt:
