780431a5d539ce456445a7462051af1bd8236098
Shiva Nathan <16snathan@gmail.com>
2020-04-06 21:37:58 -0400

    Fixed bugs with incorrect serial reading

--- raspi/raspi-decawave-serial.py
+++ raspi/raspi-decawave-serial.py
@@ -3,16 +3,19 @@ import io
 import time
 
 def main(filename,pointMarker="Reception #"):
+    fh = open(filename,"w+")
     try:
-        fh = open(filename,"w+")
         with serial.Serial('/dev/ttyACM0', 115200, timeout=0) as ser:
             sio = io.TextIOWrapper(io.BufferedRWPair(ser, ser))
             while True:
-                if sio.readline.startswith(pointMarker):
-                    fh.write("RPI_rx_ts_nanosec: %d\r\n", %time.clock_gettime_ns(time.CLOCK_MONOTONIC_RAW))
+                if sio.readline().startswith(pointMarker):
+                    fh.write("RPI_rx_ts_nanosec:" + str(time.clock_gettime_ns(time.CLOCK_MONOTONIC_RAW)) + "\r\n")
                 fh.write(sio.readline())
     except KeyboardInterrupt:
+        fh.close()
         printf(filename + "closed!")
-        close(filename)
 
+filename = sys.argv[1]
+pointMarker = sys.argv[2]
+main(filename,pointMarker)
 
