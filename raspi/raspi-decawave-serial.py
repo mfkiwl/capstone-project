7e03a678cfe3912b54b90dd29572a3391f2fca94
Shiva Nathan <16snathan@gmail.com>
2020-04-07 00:29:36 -0400

    Adding timers to check execution time

--- raspi/raspi-decawave-serial.py
+++ raspi/raspi-decawave-serial.py
@@ -10,10 +10,13 @@ def main(filename,pointMarker="Reception"):
         with serial.Serial('/dev/ttyACM0', 115200, timeout=0) as ser:
             sio = io.TextIOWrapper(io.BufferedRWPair(ser, ser))
             while True:
+                t1 = time.clock_gettime_ns(time.CLOCK_MONOTONIC_RAW)
                 line = sio.readline()
                 fh.write(line)
                 if line.startswith(pointMarker):
                     fh.write("RPI_rx_ts_nanosec:" + str(time.clock_gettime_ns(time.CLOCK_MONOTONIC_RAW)) + "\r\n")
+                t2 = time.clock_gettime_ns(time.CLOCK_MONOTONIC_RAW)
+                print("time: " + str(t2 - t1))
     except KeyboardInterrupt:
         fh.close()
         print(filename + " closed!")
