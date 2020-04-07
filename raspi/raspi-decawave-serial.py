5e36c04baf806b1e760d1c672569ca4b4cacf90e
Shiva Nathan <16snathan@gmail.com>
2020-04-07 00:40:27 -0400

    Added functions to time call to forwardLine

--- raspi/raspi-decawave-serial.py
+++ raspi/raspi-decawave-serial.py
@@ -2,6 +2,13 @@ import serial
 import io
 import time
 import sys
+from timeit import timeit
+
+# function wrapper to use for timeit, taken from https://www.pythoncentral.io/time-a-python-function/
+def wrapper(func, *args, **kwargs):
+    def wrapped():
+        return func(*args, **kwargs)
+    return wrapped
 
 #forwards line from SIO to FH. writes additional line if it spots a line that starts with pointMarker
 def forwardLine(fh,sio, pointMarker): 
@@ -16,8 +23,10 @@ def main(filename,pointMarker="Reception"):
     try:
         with serial.Serial('/dev/ttyACM0', 115200, timeout=0) as ser:
             sio = io.TextIOWrapper(io.BufferedRWPair(ser, ser))
+            timeForwardLine = wrapper(forwardLine,fh,sio,pointMarker) # 
             while True:
-                forwardLine(fh,sio,pointMarker)
+                print(timeit(timeForwardLine))
+                #forwardLine(fh,sio,pointMarker)
     except KeyboardInterrupt:
         fh.close()
         print(filename + " closed!")
