5395f3009172569cc2bb9711c191044d25b209df
Shiva Nathan <16snathan@gmail.com>
2020-04-06 21:34:23 -0400

    Modified to add RPi timestamp in real-time

--- raspi/raspi-decawave-serial.py
+++ raspi/raspi-decawave-serial.py
@@ -1,11 +1,18 @@
 import serial
 import io
-try:
-    with serial.Serial('/dev/ttyACM0', 115200, timeout=0) as ser:
-        sio = io.TextIOWrapper(io.BufferedRWPair(ser, ser))
-        while True:
-            print(sio.readline())
-except KeyboardInterrupt:
-    pass
+import time
+
+def main(filename,pointMarker="Reception #"):
+    try:
+        fh = open(filename,"w+")
+        with serial.Serial('/dev/ttyACM0', 115200, timeout=0) as ser:
+            sio = io.TextIOWrapper(io.BufferedRWPair(ser, ser))
+            while True:
+                if sio.readline.startswith(pointMarker):
+                    fh.write("RPI_rx_ts_nanosec: %d\r\n", %time.clock_gettime_ns(time.CLOCK_MONOTONIC_RAW))
+                fh.write(sio.readline())
+    except KeyboardInterrupt:
+        printf(filename + "closed!")
+        close(filename)
 
 
