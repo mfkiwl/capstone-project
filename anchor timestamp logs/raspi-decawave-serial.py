2508a6ce3809c4c85ad1076b1b9aa3fbe482d769
Shiva Nathan <16snathan@gmail.com>
2020-04-07 23:53:19 -0400

    Moved raspi-decawave-serial.py to anchor timestamp logs

--- anchor timestamp logs/raspi-decawave-serial.py
+++ anchor timestamp logs/raspi-decawave-serial.py
@@ -1,11 +1,29 @@
 import serial
 import io
-try:
-    with serial.Serial('/dev/ttyACM0', 115200, timeout=0) as ser:
-        sio = io.TextIOWrapper(io.BufferedRWPair(ser, ser))
-        while True:
-            print(sio.readline())
-except KeyboardInterrupt:
-    pass
+import time
+import sys
+from timeit import timeit
 
+#forwards line from SIO to FH. writes additional line if it spots a line that starts with pointMarker
+def forwardLine(fh,sio, pointMarker): 
+    line = sio.readline()
+    fh.write(line)
+    if line.startswith(pointMarker):
+        fh.write("RPI_rx_ts_nanosec:" + str(time.clock_gettime_ns(time.CLOCK_MONOTONIC_RAW)) + '\n')
+
+def main(filename,pointMarker="Reception"):
+    fh = open(filename,'w+')
+    print("filename opened!")
+    try:
+        with serial.Serial('/dev/ttyACM0', 115200, timeout=0) as ser:
+            sio = io.TextIOWrapper(io.BufferedRWPair(ser, ser))
+            while True:
+                forwardLine(fh,sio,pointMarker)
+    except KeyboardInterrupt:
+        fh.close()
+        print(filename + " closed!")
+
+filename = str(sys.argv[1])
+pointMarker = str(sys.argv[2])
+main(filename,pointMarker)
 
