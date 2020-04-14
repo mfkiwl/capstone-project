72abd8b48a6049f5a289b205742e1040062e3c1f
Shiva Nathan <16snathan@gmail.com>
2020-04-13 21:29:43 -0400

    As part of rewrite of how this program reads log, eliminated unnecessary command-line parameters & functions

--- anchor timestamp logs/raspi-decawave-serial.py
+++ anchor timestamp logs/raspi-decawave-serial.py
@@ -4,14 +4,7 @@ import time
 import sys
 from timeit import timeit
 
-#forwards line from SIO to FH. writes additional line if it spots a line that starts with pointMarker
-def forwardLine(fh,sio, pointMarker): 
-    line = sio.readline()
-    fh.write(line)
-    if line.startswith(pointMarker):
-        fh.write("RPI_rx_ts_nanosec:" + str(time.clock_gettime_ns(time.CLOCK_MONOTONIC_RAW)) + '\n')
-
-def main(filename,pointMarker="Reception"):
+def main(filename):
     fh = open(filename,'w+')
     print("filename opened!")
     try:
@@ -37,6 +30,5 @@ def main(filename,pointMarker="Reception"):
         print(filename + " closed!")
 
 filename = str(sys.argv[1])
-pointMarker = str(sys.argv[2])
-main(filename,pointMarker)
+main(filename)
 
