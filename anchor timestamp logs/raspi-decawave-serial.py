cea2da1d4a826e1cd38adcfd2941d197939c552d
Shiva Nathan <16snathan@gmail.com>
2020-04-21 17:19:08 -0400

    Updated to log different things

--- anchor timestamp logs/raspi-decawave-serial.py
+++ anchor timestamp logs/raspi-decawave-serial.py
@@ -7,47 +7,91 @@ from timeit import timeit
 def main(filename, serial_port):
     fh = open(filename,'w+')
     print("filename opened!")
+    switch = "Tag"
     try:
         with serial.Serial(serial_port, 115200, timeout=1) as ser:
             sio = io.TextIOWrapper(io.BufferedRWPair(ser, ser))
-            receptionNum = "N/A"
+
+            receptionNum = -1
             pulseNum = -1
-            RPItimeNS = -1
-            # DECAtime = -1
-            DECAtimeNS = -1
-            anchorID = "N/A"
-            tagID = "N/A"
+            syncNum = -1
+
+            tS = -1
+            tSnew = -1
+            tM = -1
+            tMnew = -1
+            tMraw = -1
+            R = -1
+
+            DECAtime = -1
+            dT = -1
+            syncT = -1
+
             while True:
                 line = sio.readline()
 
                 if line.startswith("TimeOut"): continue
                 if "Error" in line: continue
 
-                if line.startswith("Reception"): # Retrieve the frame number as an INT & get the RPI current time
+                if line.startswith("Master"): # Write sync pulse from Master 
+                    switch = "Master"
+                if line.startswith("Tag"):  # Write data pulse from Tag 
+                    switch = "Tag"
+
+                if line.startswith("Reception"): # Retrieve the frame number as an INT & 
                     receptionNum = int(line.split(":")[1].strip())
-                    RPItimeNS = time.clock_gettime_ns(time.CLOCK_MONOTONIC_RAW)
                 if line.startswith("Pulse"): # Retrieve the tag pulse number as an INT for synchronization
                     pulseNum = int(line.split(":")[1].strip())
-                if line.startswith("sync_ts_nanosec"): # Retrieve the synchronized decawave time
-                    DECAtimeNS = int(line.split(":")[1].strip())
-                    # print("DECAtimeNS:" + str(DECAtimeNS))
-                if line.startswith("anchor"): # Retrieve the anchor ID as a STRING
-                    anchorID = line.split(":")[1].strip()
-                    # print("anchorID:" + str(anchorID))
-                if line.startswith("tag"):  # Retrieve the tag ID as a STRING
-                    tagID = line.split(":")[1].strip()
-                    # print("tagID:" + str(tagID))
+                if line.startswith("Sync"): # Retrieve the tag pulse number as an INT for synchronization
+                    syncNum = int(line.split(":")[1].strip())
+
+                if line.startswith("tS:"): # Retrieve the 
+                    tS = int(line.split(":")[1].strip())
+
+                if line.startswith("tSnew:"): # Retrieve the 
+                    tSnew = int(line.split(":")[1].strip())
+
+                if line.startswith("tM:"): # Retrieve the 
+                    tM = int(line.split(":")[1].strip())
+
+                if line.startswith("tMnew:"): # Retrieve the 
+                    tMnew = int(line.split(":")[1].strip())
+
+                if line.startswith("tMraw:"): # Retrieve the 
+                    tMraw = int(line.split(":")[1].strip())
+
+                if line.startswith("R:"): # Retrieve the 
+                    R = int(line.split(":")[1].strip())    
+
+                if line.startswith("T:"): # Retrieve the synchronized decawave time
+                    DECAtime = int(line.split(":")[1].strip())
+                if line.startswith("dT:"): # Retrieve the synchronized decawave time
+                    dT = int(line.split(":")[1].strip())
+                if line.startswith("syncT:"): # Retrieve the synchronized decawave time
+                    syncT = int(line.split(":")[1].strip())
 
                 if line.startswith("END"): # Reached end of frame, write fields & reset 
-                    fh.write("Reception #: {}\nPulse #: {}\nAnchor ID: {}\nTag ID: {}\nRPI Time(NS): {}\nDECAWAVE Synced Time(NS): {}\n\n".format(
-                        receptionNum, pulseNum, anchorID, tagID, RPItimeNS, DECAtimeNS))
-                    receptionNum = ""
+                    if (switch == "Master"):
+                        fh.write("Master Sync\nReception: {}\nSync: {}\ntS: {}\ntSnew: {}\ntM: {}\ntMnew: {}\ntMraw: {}\nR: {}\n\n".format(
+                        receptionNum, syncNum, tS, tSnew, tM, tMnew, tMraw, R))
+                    if (switch == "Tag"):
+                        fh.write("Tag Pulse\nReception: {}\nPulse: {}\nT: {}\ndT: {}\nsyncT: {}\n\n".format(
+                        receptionNum, pulseNum, DECAtime, dT, syncT))
+                    receptionNum = -1
                     pulseNum = -1
-                    RPItimeNS = -1
+                    RPItime = -1
+                    tS = -1
+                    tSnew = -1
+                    tM = -1
+                    tMnew = -1
+                    tMraw = -1
+                    R = -1
                     DECAtime = -1
-                    DECAtimeNS = -1
-                    anchorID = ""
-                    tagID = ""
+                    dT = -1
+                    syncT = -1
+                    anchorID = "N/A"
+                    tagID = "N/A"
+
     except KeyboardInterrupt:
         fh.close()
         print(filename + " closed!")
