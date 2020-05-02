fc92829e4985d97ef40a1c8493fcb2973e1570d5
Udit Ranasaria <uditrana@outlook.com>
2020-05-02 02:10:08 -0400

    updates to collect data for all players

--- Simulator+Localizer/filterNBAData.py
+++ Simulator+Localizer/filterNBAData.py
@@ -1,7 +1,9 @@
 import pickle
 import csv
-import pandas
+import os
 import pprint
+import time
+from collections import defaultdict
 from basketball_mapping import *
 
 pp = pprint.PrettyPrinter(indent=2)
@@ -13,37 +15,57 @@ export_headers = ["Sample_idx", "timestamp_ms", "adjusted_game_time", "player_id
 
 steph = 201939
 max_clock = 720.0*4
-sample_interval = 100 #milliseconds
+SAMPLE_INTERVAL = 100 #milliseconds
 
-data = pickle.load(open("test_data/0021500003-NOPatGSW-2015-10-27.pickle", "rb"))
-timedData = []
+def getPlayerString(pid):
+    return f"{players[player]['firstname']}_{players[player]['lastname']}_{players[player]['jersey']}_{players[player]['position']}"
+startT = time.time()
+gameFile = "0021500003-NOPatGSW-2015-10-27"
+data = pickle.load(open(f"test_data/{gameFile}.pickle", "rb"))
+players = pickle.load(open("test_data/0_players.pickle", 'rb'))
 
+playerToSamples = defaultdict(lambda: [])
+
+t = time.time()
+counter = 0
 for moment in data:
-    if moment[8]!=steph: continue
+    pid = moment[8]
+    if pid==-1: continue
+    # if pid!=steph: continue
     quarter, game_clock = moment[3], moment[5]
     adjustedTime = (4-quarter)*720.0 + game_clock
     footX, footY = moment[9], moment[10]
     meterX, meterY = footToMeter(footX), footToMeter(footY)
-    timedData.append([int(moment[4]%1e10), round(adjustedTime,2), moment[8], round(meterX, 4), round(meterY, 4)])
+    playerToSamples[pid].append([int(moment[4]%1e10), round(adjustedTime,2), moment[8], round(meterX, 4), round(meterY, 4)])
+    counter+=1
+    if counter%100000==0:
+        # print(f"Processed {counter} moments in {time.time()-t} seconds")
+        t = time.time()
 
-timedData.sort(key=lambda x: x[0])
+for samples in playerToSamples.values():
+    samples.sort(key=lambda x: x[0])
 
-finalData = []
+t = time.time()
+for player, samples in playerToSamples.items():
+    downsampledSamples = []
+    timestamp = samples[0][0]
+    for sample in samples:
+        if sample[0] >= timestamp:
+            downsampledSamples.append(sample)
+            timestamp = sample[0] + SAMPLE_INTERVAL
+    playerToSamples[player] = downsampledSamples
+    # print(f"Processed {getPlayerString(player)} moments into {len(downsampledSamples)} samples in {time.time()-t} seconds")
+    t = time.time()
 
-timestamp = timedData[0][0]
-for moment in timedData:
-    if moment[0] >= timestamp:
-        finalData.append(moment)
-        timestamp = moment[0] + sample_interval
+for player, samples in playerToSamples.items():
+    playerString = getPlayerString(player)
+    dir = f"test_data/{gameFile}"
+    if not os.path.exists(dir):
+        os.makedirs(dir)
+    with open(f"{dir}/{playerString}.csv", "w+", newline='') as f:
+        cW = csv.writer(f)
+        cW.writerow(export_headers)
+        for i, row in enumerate(samples):
+            cW.writerow([i]+row)
 
-with open("test_data/stephDownSampled.csv", "w+", newline='') as f:
-    cW = csv.writer(f)
-    cW.writerow(export_headers)
-    for i, row in enumerate(finalData):
-        cW.writerow([i]+row)
-
-
-# df = pandas.DataFrame(data, columns=movement_headers)
-# # pprint(data[:10])
-# df = df.filter(items=["quarter", "game_clock", "player_id", "x_loc", "y_loc"])
-# print(df.head(5))
+print(f"DONE. Took {time.time()-startT} seconds")
