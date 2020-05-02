658c729b486d54a5fcb1dd3b764fb73cf34347fd
Shiva Nathan <16snathan@gmail.com>
2020-05-02 00:27:47 -0400

    Added check to skip reading master-sync pulses

--- anchor timestamp logs/demo-slave.py
+++ anchor timestamp logs/demo-slave.py
@@ -8,6 +8,7 @@ with serial.Serial(str(sys.argv[1]), 115200, timeout=1) as ser:
     anchorID = ""
     pulseNum = -1
     syncT = -1
+    anchorPulse = False
 
     while True:
         line = sio.readline()
@@ -17,6 +18,7 @@ with serial.Serial(str(sys.argv[1]), 115200, timeout=1) as ser:
         try:
             if line.startswith("Anchor"):
                 anchorID = line.split(":")[1].strip()
+                anchorPulse = True
 
             if line.startswith("Pulse"): # Retrieve the tag pulse number as an INT for synchronization
                 pulseNum = int(line.split(":")[1].strip())
@@ -24,7 +26,7 @@ with serial.Serial(str(sys.argv[1]), 115200, timeout=1) as ser:
             if line.startswith("syncT:"): # Retrieve the synchronized decawave time
                 syncT = int(line.split(":")[1].strip())
 
-            if line.startswith("END"): # Reached end of frame, write fields & reset 
+            if line.startswith("END") and anchorPulse: # Reached end of frame, write fields & reset 
                 # print("anchor {} Pulse {} syncT {}\n".format(anchorID,pulseNum,syncT))
                 sys.stdout.write("anchor {} Pulse {} syncT {}\n".format(anchorID,pulseNum,syncT))
                 sys.stdout.flush()
