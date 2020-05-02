9f8a65a5e1c695a39e0781cb773aa1555d6bbdbb
Shiva Nathan <16snathan@gmail.com>
2020-05-02 00:50:32 -0400

    Trying to account for botched reads

--- anchor timestamp logs/demo-master.py
+++ anchor timestamp logs/demo-master.py
@@ -6,14 +6,23 @@ p2 = Popen(['python', 'demo-slave.py', "/dev/ttyACM2"], stdin=PIPE, stdout=PIPE,
 p3 = Popen(['python', 'demo-slave.py', "/dev/ttyACM3"], stdin=PIPE, stdout=PIPE, stderr=PIPE)
 
 while True:
+    successes = 0
     out0 = p0.stdout.readline().decode('utf-8').split(" ")
-    print("out0: {}".format(out0))
-    (anchor0,pulse0,syncT0) = (out0[1],out0[3],out0[5])
+    if len(out0) > 5:
+        (anchor0,pulse0,syncT0) = (out0[1],out0[3],out0[5])
+        successes += 1
     out1 = p1.stdout.readline().decode('utf-8').split(" ")
-    (anchor1,pulse1,syncT1) = (out1[1],out1[3],out1[5])
+    if len(out1) > 5:
+        (anchor1,pulse1,syncT1) = (out1[1],out1[3],out1[5])
+        successes += 1
     out2 = p2.stdout.readline().decode('utf-8').split(" ")
-    (anchor2,pulse2,syncT2) = (out2[1],out2[3],out2[5])
+    if len(out2) > 5: 
+        (anchor2,pulse2,syncT2) = (out2[1],out2[3],out2[5])
+        successes += 1
     out3 = p3.stdout.readline().decode('utf-8').split(" ")
-    (anchor3,pulse3,syncT3) = (out3[1],out3[3],out3[5])
-    print("out0:{}\nout1:{}\nout2: {}\nout3: {}\n\n"
+    if len(out3) > 5: 
+        (anchor3,pulse3,syncT3) = (out3[1],out3[3],out3[5])
+        successes += 1
+    if successes >= 4:
+        print("out0:{}\nout1:{}\nout2: {}\nout3: {}\n\n"
         .format((anchor0,pulse0,syncT0), (anchor1,pulse1,syncT1), (anchor2,pulse2,syncT2),(anchor3,pulse3,syncT3)))
